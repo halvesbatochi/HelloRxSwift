dbd83fd12c9d9a8d8c18d502c4ab901a34faf156
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-10 12:25:33 -0300

    Filtering operators - Take

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -208,3 +208,13 @@ import PlaygroundSupport
 //trigger.onNext("X")
 //
 //subject.onNext("C")
+
+// TAKE
+//
+//let disposeBag = DisposeBag()
+//
+//Observable.of(1,2,3,4,5,6)
+//    .take(3)
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
