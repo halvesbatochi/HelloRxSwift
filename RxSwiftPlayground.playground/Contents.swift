bd86638b64fcfae74ccc4a08790568e5eae53b90
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-10 12:28:57 -0300

    Filtering operators - Take until

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -218,3 +218,16 @@ import PlaygroundSupport
 //    .subscribe(onNext: {
 //        print($0)
 //    }).disposed(by: disposeBag)
+
+// TAKE WHILE
+//
+//let disposeBag = DisposeBag()
+//
+//Observable.of(2,4,6,7,8,10)
+//    .takeWhile {
+//        return $0 % 2 == 0
+//    }.subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
+//
+//
