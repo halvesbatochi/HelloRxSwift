128a8cb7156c96da2de1595183d1b522755579fd
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-10 12:23:26 -0300

    Filtering operators - Skip until

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -182,10 +182,29 @@ import PlaygroundSupport
 
 // SKIP WHILE
 //
-let disposeBag = DisposeBag()
+//let disposeBag = DisposeBag()
+//
+//Observable.of(2,2,3,4,4)
+//    .skipWhile { $0 % 2 == 0 }
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
 
-Observable.of(2,2,3,4,4)
-    .skipWhile { $0 % 2 == 0 }
-    .subscribe(onNext: {
-        print($0)
-    }).disposed(by: disposeBag)
+// SKIP UNTIL
+//
+//let disposeBag = DisposeBag()
+//
+//let subject = PublishSubject<String>()
+//let trigger = PublishSubject<String>()
+//
+//subject.skipUntil(trigger)
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
+//
+//subject.onNext("A")
+//subject.onNext("B")
+//
+//trigger.onNext("X")
+//
+//subject.onNext("C")
