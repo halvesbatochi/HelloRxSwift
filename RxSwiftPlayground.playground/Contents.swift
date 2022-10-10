0a2a384acd5908ffdb5ece188fea93c7fcc40e83
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-10 12:18:49 -0300

    Filtering operators - Skip while

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -179,3 +179,13 @@ import PlaygroundSupport
 //    .subscribe(onNext: {
 //        print($0)
 //    }).disposed(by: disposeBag)
+
+// SKIP WHILE
+//
+let disposeBag = DisposeBag()
+
+Observable.of(2,2,3,4,4)
+    .skipWhile { $0 % 2 == 0 }
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
