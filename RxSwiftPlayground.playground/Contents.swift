dcb7ac7fa6903e1da1d22c0204d3725cd3606adc
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-11 10:18:23 -0300

    Transforming operators - Flat map

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -278,3 +278,29 @@ import PlaygroundSupport
 //        print($0)
 //    }).disposed(by: disposeBag)
 
+// FLAT MAP
+//
+//let disposeBag = DisposeBag()
+//
+//struct Student {
+//    var score: BehaviorRelay<Int>
+//}
+//
+//let john = Student(score: BehaviorRelay(value: 75))
+//let mary = Student(score: BehaviorRelay(value: 95))
+//
+//let student = PublishSubject<Student>()
+//
+//student.asObservable()
+//    .flatMap { $0.score.asObservable() }
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
+//
+//student.onNext(john)
+//john.score.accept(100)
+//
+//student.onNext(mary)
+//mary.score.accept(80)
+//
+//john.score.accept(43)
