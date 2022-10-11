9cec00146447bc416b6c9c638620c3947e940fa8
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-11 16:39:25 -0300

    Combining operators - Merge

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -350,12 +350,31 @@ import PlaygroundSupport
 
 // CONCAT
 //
+//let disposeBag = DisposeBag()
+//
+//let first = Observable.of(1,2,3)
+//let second = Observable.of(4,5,6)
+//
+//let observable = Observable.concat([first, second])
+//observable.subscribe(onNext: {
+//    print($0)
+//}).disposed(by: disposeBag)
+
+// MERGE
+//
 let disposeBag = DisposeBag()
 
-let first = Observable.of(1,2,3)
-let second = Observable.of(4,5,6)
+let left = PublishSubject<Int>()
+let right = PublishSubject<Int>()
 
-let observable = Observable.concat([first, second])
+let source = Observable.of(left.asObservable(),right.asObservable())
+let observable = source.merge()
 observable.subscribe(onNext: {
     print($0)
 }).disposed(by: disposeBag)
+
+left.onNext(5)
+left.onNext(3)
+right.onNext(2)
+right.onNext(1)
+left.onNext(99)
