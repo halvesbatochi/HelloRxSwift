82143806410c5a663270fed0b2a0908898279421
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-11 16:33:19 -0300

    Combining operators - Concat

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -338,10 +338,24 @@ import PlaygroundSupport
 //-------------------------------------------------//
 // SECTION 7: COMBINING OPERATORS                  //
 //-------------------------------------------------//
+//
+// STARTS WITH
+//
+//let disposeBag = DisposeBag()
+//let numbers = Observable.of(2,3,4)
+//let observable = numbers.startWith(1)
+//observable.subscribe(onNext: {
+//    print($0)
+//}).disposed(by: disposeBag)
 
+// CONCAT
+//
 let disposeBag = DisposeBag()
-let numbers = Observable.of(2,3,4)
-let observable = numbers.startWith(1)
+
+let first = Observable.of(1,2,3)
+let second = Observable.of(4,5,6)
+
+let observable = Observable.concat([first, second])
 observable.subscribe(onNext: {
     print($0)
 }).disposed(by: disposeBag)
