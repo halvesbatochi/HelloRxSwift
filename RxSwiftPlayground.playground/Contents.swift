ab45e1a79795f1e398adbe84eabfb45e4f8f8101
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-11 10:11:36 -0300

    Transforming Operators - Map

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -259,12 +259,22 @@ import PlaygroundSupport
 
 // TO ARRAY
 //
-let disposeBag = DisposeBag()
+//let disposeBag = DisposeBag()
+//
+//Observable.of(1,2,3,4,5)
+//    .toArray()
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
 
-Observable.of(1,2,3,4,5)
-    .toArray()
-    .subscribe(onNext: {
-        print($0)
-    }).disposed(by: disposeBag)
-
+// MAP
+//
+//let disposeBag = DisposeBag()
+//
+//Observable.of(1,2,3,4,5)
+//    .map {
+//        return $0 * 2
+//    }.subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
 
